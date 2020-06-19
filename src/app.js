fa3b40b6111b86005e3f15532c2877473b4709ff
jripke74 <jripke74@gmail.com>
2020-06-19 13:53:38 -0700

    add clipboard function

--- src/app.js
+++ src/app.js
@@ -1,5 +1,18 @@
 const button = document.querySelector('button');
+const textParagraph = document.querySelector('p');
 
 button.addEventListener('click', () => {
-  // do something...
+  if (navigator.clipboard) {
+    const text = textParagraph.textContent;
+    navigator.clipboard
+      .writeText(text)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  } else {
+    alert('Feature not available, please copy manually!');
+  }
 });
